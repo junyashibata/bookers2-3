0931a2ebd9bc5a611df692dda01a3eadb906a896
JunyaShibata <135484706+junyashibata@users.noreply.github.com>
2023-06-24 13:22:09 +0000

    booksshow

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,24 @@
 class BooksController < ApplicationController
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book)
   end
-  
+
   def index
-    @book = Book.new
+    @book_new = Book.new
     @books = Book.all
   end
-  
+
+  def show
+    @book_new = Book.new
+    @book = Book.find(params[:id])
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :opinion)
   end
